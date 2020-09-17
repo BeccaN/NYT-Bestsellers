c853e6b51a98f8c8455550b2289dafb4c4295b77
rncongi <rncongi@gmail.com>
2020-09-17 15:35:47 -0400

    added colorize gem

--- config/environment.rb
+++ config/environment.rb
@@ -6,7 +6,6 @@ require 'net/http'
 require 'openssl'
 require 'json'
 
-
 require_relative '../lib/adapter.rb'
 require_relative '../lib/book.rb'
 require_relative '../lib/list.rb'
--- lib/cli.rb
+++ lib/cli.rb
@@ -7,7 +7,7 @@ class CLI
     end 
 
     def greet_user
-        puts "Greetings Bookworm! Lets take a look at current NY Times Hardcover Fiction bestsellers!"
+        puts "Greetings Bookworm! Lets take a look at current NY Times Hardcover Fiction bestsellers!".colorize(:blue)
     end 
     
     def fetch_books
@@ -28,7 +28,7 @@ class CLI
             puts "
 This list was established on #{list_array[1]}/#{list_array[2]}/#{list_array[0]}!
 ----------------------------------------
-"
+".colorize(:blue)
         end 
 
         Book.all.each do |ind_book|
@@ -38,11 +38,11 @@ This list was established on #{list_array[1]}/#{list_array[2]}/#{list_array[0]}!
 
     def book_prompt
         puts "----------------------------------------
-Please input the number of the book you wish to learn more about."
+Please input the number of the book you wish to learn more about.".colorize(:blue)
         input = gets
 
         until input.to_i.between?(1,15)
-            puts "Please enter a number between 1-15."
+            puts "Please enter a number between 1-15.".colorize(:red)
             input = gets
         end 
 
@@ -59,14 +59,14 @@ Weeks on list: #{Book.all[input_index].weeks_on_list} | Published by: #{Book.all
         puts "
 --MENU--
 1. Books List
-2. Exit
-
+2. Exit"
+        puts "
 Please enter number 1 or 2 to continue...
-"
+".colorize(:blue)
         input = gets
 
         until input.to_i == 1 || input.to_i == 2
-            puts "You must either type in 1(books list) or 2(exit) to continue..."
+            puts "You must either type in 1(books list) or 2(exit) to continue...".colorize(:red)
             input = gets
         end
 
